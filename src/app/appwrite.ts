959391342e64c11f1e080975186de78409c79c34
Mathis Vester <26164587+mathisvester@users.noreply.github.com>
2024-02-15 19:08:27 +0100

    chore: update appwrite project id

--- src/app/appwrite.ts
+++ src/app/appwrite.ts
@@ -4,7 +4,7 @@ export const client = new Client();
 
 client
   .setEndpoint('https://cloud.appwrite.io/v1')
-  .setProject('65a259553ebaac57544f');
+  .setProject('65ce50b4e68545c2f9b1');
 
 export const account = new Account(client);
 export { ID } from 'appwrite';
